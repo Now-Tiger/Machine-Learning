473aaf6b93b74ce7c144e36cb092a7b57d6378ca
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-06-28 02:08:56 +0530

    Add files via upload

---
+++ programs/Revs_by_one.py
@@ -0,0 +1,23 @@
+
+# Function to left rotate an array by d = d is index number
+def leftrotate(arr, d, n):            
+    for i in range(d):
+        leftByOne(arr, n)
+
+def leftByOne(arr, n):
+    temp = arr[0]
+    for i in range(n-1):
+        arr[i] = arr[i + 1]
+    arr[n - 1] = temp
+
+def printArray(arr, size):
+    for i in range(size):
+        print("% d"% arr[i], end =" ")
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+leftrotate(arr, 2, 7)
+printArray(arr, 7) 
+
+arr = [2, 8, 5, 6, 1, 3, 4]
+leftrotate(arr, 4, 7)
+printArray(arr, 7)
